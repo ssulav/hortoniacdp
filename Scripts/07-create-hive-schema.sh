e26d02b92d344a8a90d35a61fcab66bd4c8368b2
Soumitra Sulav <ssulav@cloudera.com>
2019-08-23 11:01:24 +0530

    updated directory name

--- Scripts/07-create-hive-schema.sh
+++ Scripts/07-create-hive-schema.sh
@@ -1,7 +1,7 @@
 source config.ini
 
-beeline --hivevar SFS_URL=${SFS_URL} -f ${DATA_DIR}/HiveSchema.hsql
+beeline --hivevar SFS_URL=${SFS_URL} -f ${SCRIPTS_DIR}/HiveSchema.hsql
 
 if [ "${ACID}" = true  ]; then
-  beeline iSFS_URL=${SFS_URL} -f ${DATA_DIR}/TransSchema.hsql
+  beeline --hivevar SFS_URL=${SFS_URL} -f ${SCRIPTS_DIR}/TransSchema.hsql
 fi
